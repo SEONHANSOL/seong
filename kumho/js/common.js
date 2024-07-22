cf4599939625b8bcd007a42fb24e287a458cc1b6
SEONHANSOL <goodgirl48@naver.com>
2024-07-22 13:48:50 +0900

    240722 금호건설

--- kumho/js/common.js
+++ kumho/js/common.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
         resize_chk()
     }) // $(window).resize
 
-    $('header').on('mouseenter', function(){
+    $('header').on('mouseenter focusin', function(){
         $(this).addClass('fixed')
     })
     $('header').on('mouseleave', function(){
@@ -58,7 +58,7 @@ $(document).ready(function(){
         scroll_chk()
     })
 
-    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
         if(pc_mobile == 'pc'){
             $('header').addClass('memu_over')
             $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
@@ -71,6 +71,13 @@ $(document).ready(function(){
             $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
         }
     })
+    $('header .gnb .gnb_wrap ul.depth1 > li:last-child > ul.depth2 > li:last-child > a ').on('focusout', function(){
+        if(pc_mobile == 'pc'){
+            $('header').removeClass('memu_over')
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
+        }
+    })
+    
 
     /* 
         모바일 메뉴를 클릭하면
