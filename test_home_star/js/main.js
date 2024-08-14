8921c169c29a97b6ab31fb5a113ad91aa5df2d9e
SEONHANSOL <goodgirl48@naver.com>
2024-08-14 22:03:50 +0900

    -

--- test_home_star/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('header .gnb .depth1 > li').on('mouseenter focusin', function(){
-        $('header .gnb .depth1 > li').removeClass('over')
-        $(this).addClass('over')
-    })
-    $('header .gnb').on('mouseleave', function(){
-        $('header .gnb .depth1 > li').removeClass('over')
-    }) // header메뉴 마우스오버 구현
-}) 
