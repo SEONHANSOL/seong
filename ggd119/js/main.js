e9ea06ef496d8863ec9c578dbec20215f98ee3d1
SEONHANSOL <goodgirl48@naver.com>
2024-09-23 18:00:35 +0900

    240923 포폴 고양소방서

--- ggd119/js/main.js
+++ ggd119/js/main.js
@@ -109,7 +109,7 @@ $(document).ready(function(){
   })
 
 
-  const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+  const popup_swiper = new Swiper('.popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
     autoplay: {  /* 팝업 자동 실행 */
         delay: 5000,
@@ -119,81 +119,33 @@ $(document).ready(function(){
     loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
     pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-        el: '.visual .paging', /* 해당 요소의 class명 */
+        el: '.popup .paging', /* 해당 요소의 class명 */
         clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        type: 'fraction',
+        renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+		    return '<span class="' + className + '">' + (index + 1) + "</span>";
+		},
     },
     
 
     navigation: {  /* 이전, 다음 버튼 */
-        nextEl: '.visual .btn_next',  /* 다음 버튼의 클래스명 */
-        prevEl: '.visual .btn_prev',  
+        nextEl: '.popup .btn_next',  /* 다음 버튼의 클래스명 */
+        prevEl: '.popup .btn_prev',  
     },
 
   });
-  visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-  visual_swiper.autoplay.start();  /* 재생 기능 */
+  popup_swiper.autoplay.stop();  /* 일시정지 기능 */
+  popup_swiper.autoplay.start();  /* 재생 기능 */
 
-  /*
-  .visual .btn_wrap button.btn_stop
-  일시정지버튼을 클릭하면 
-  1. 팝업 일시정지
-  2. 일시정지 버튼은 숨김, 재생버튼 나타남
-
-  .visual .btn_wrap button.btn_play
-  재생버큰을 클릭하면
-  1. 팝업은 다시시작
-  2. 재생버튼은 숨김, 일시정지 버튼은 나타남
-  */
-
-  $('.visual .btn_wrap button.btn_stop').on('click', function(){
-    visual_swiper.autoplay.stop();
+  $('.container .main_inner .popup button.btn_stop').on('click', function(){
+    popup_swiper.autoplay.stop();
     $(this).hide() //숨김
-    $('.visual .btn_wrap button.btn_play').show() //보이기
+    $('.container .main_inner .popup button.btn_play').show() //보이기
   })
-  $('.visual .btn_wrap button.btn_play').on('click', function(){
-      visual_swiper.autoplay.start();
-      $(this).hide() //숨김
-      $('.visual .btn_wrap button.btn_stop').show() //보이기
-  })
-
-  const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
-    slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-    spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-    breakpoints: {
-      700: {   /* 1024px 이상일때 적용 */
-        slidesPerView: 'auto',
-        spaceBetween: 16,
-      },
-    },
-    loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    autoplay: {  /* 팝업 자동 실행 */
-      delay: 2500,
-      disableOnInteraction: true,
-    },
-  });
-
-  /*
-  .subject .list ul li 한테 오버한 li에만 active 클래스를 추가
-  이전에 active를 가지고 있었던 li에서는 active클래스를 삭제할것임
-
-  이전에 오버한 li는 알기 어려움
-  그래서 모든 li에 있는 active 클래스를 모두 삭제하고,
-  오버한 li에만 다시 active 클래스를 추가 할 것임
-  */
-
-  $('.subject .list ul li').on('mouseenter', function(){
-    $('.subject .list ul li').removeClass('active')
-    $(this).addClass('active')
+  $('.container .main_inner .popup button.btn_play').on('click', function(){
+    popup_swiper.autoplay.start();
+    $(this).hide() //숨김
+    $('.container .main_inner .popup button.btn_stop').show() //보이기
   })
 
-  const subject_swiper = new Swiper('.subject .swiper', { /* 팝업을 감싼는 요소의 class명 */
-    slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
-    spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
-    breakpoints: {
-        640: {  /* 640px 이상이 되면 적용 */
-            spaceBetween: 24, 
-        },
-    },
-    loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-});
 })
