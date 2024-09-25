a4548667a13079b40d88e448a6fa22ebed22b7dc
SEONHANSOL <goodgirl48@naver.com>
2024-09-25 18:01:31 +0900

    240925 고양경찰서

--- ggd119/js/main.js
+++ ggd119/js/main.js
@@ -219,4 +219,58 @@ $(document).ready(function(){
         $('.news_ppzone .ppzone button.btn_stop').show() //보이기
     })
 
+
+    const citizen_swiper = new Swiper('.citizen .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            1: {    /* 640px 이상일때 적용 */
+                slidesPerView: 1,
+                spaceBetween: 16,
+            },
+            400: {    /* 640px 이상일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 16,
+            },
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 16,
+            },
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
+            1024: {   /* 1024px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+            1280: {    /* 1280px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 25000,
+            disableOnInteraction: true,
+        },
+        navigation: {
+            nextEl: '.citizen .btn_next',
+            prevEl: '.citizen .btn_prev',
+        },
+    });
+    citizen_swiper.autoplay.stop();  /* 일시정지 기능 */
+    citizen_swiper.autoplay.start();  /* 재생 기능 */
+	
+    $('.citizen button.btn_stop').on('click', function(){
+        ppzone_swiper.autoplay.stop();
+        $(this).hide() //숨김
+        $('.citizen button.btn_play').show() //보이기
+    })
+    $('.citizen button.btn_play').on('click', function(){
+        ppzone_swiper.autoplay.start();
+        $(this).hide() //숨김
+        $('.citizen button.btn_stop').show() //보이기
+    })
+
 })
