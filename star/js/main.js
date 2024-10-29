bbc14675b91e2cbc04e881fb5a4f8b652294487a
SEONHANSOL <goodgirl48@naver.com>
2024-10-29 16:02:39 +0900

    1029 시험공부

--- star/js/main.js
+++ star/js/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-    $('header .gnb .depth1 > li').on('mouseenter focusin' ,function(){
+    $('header .gnb .depth1 > li').on('mouseenter focusin', function(){
         $('header .gnb .depth1 > li').removeClass('over')
         $(this).addClass('over')
     })
-    $('header .gnb').on('mouseleave focusin' ,function(){
+    $('header .gnb').on('mouseleave', function(){
         $('header .gnb .depth1 > li').removeClass('over')
+    })
+
+    $('.news .tab > li').on('click', function(){
+        $('.news .tab > li').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    $('.layer_open').on('click', function(){
+        $('.layer_popup').show()
+    })
+    $('.layer_popup button').on('click', function(){
+        $('.layer_popup').hide()
     })
 })
