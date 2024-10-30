f33033e5657d6e48e79701d7686645c9fe386088
SEONHANSOL <goodgirl48@naver.com>
2024-10-30 14:07:28 +0900

    1030 시험연습

--- star/js/main.js
+++ star/js/main.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
         $('header .gnb .depth1 > li').removeClass('over')
         $(this).addClass('over')
     })
-    $('header .gnb').on('mouseleave', function(){
+    $('header .gnb .depth1 > li').on('mouseleave', function(){
         $('header .gnb .depth1 > li').removeClass('over')
     })
 
-    $('.news .tab > li').on('click', function(){
-        $('.news .tab > li').removeClass('active')
+
+    $('.new .tab li').on('click', function(){
+        $('.new .tab li').removeClass('active')
         $(this).addClass('active')
     })
 
