98239b307b04cac3f507faf06ab886dfc107df99
SEONHANSOL <goodgirl48@naver.com>
2024-11-04 18:17:59 +0900

    d

--- star/js/main.js
+++ star/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         $('header .gnb .depth1 > li').removeClass('over')
         $(this).addClass('over')
     })
+
     $('header .gnb .depth1 > li').on('mouseleave', function(){
         $('header .gnb .depth1 > li').removeClass('over')
     })
