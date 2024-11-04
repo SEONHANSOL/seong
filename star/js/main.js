4757af11aeea6afdaf407666c157dc4b88785728
SEONHANSOL <goodgirl48@naver.com>
2024-11-04 21:26:00 +0900

    1104 시험공부

--- star/js/main.js
+++ star/js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function(){
     $('header .gnb .depth1 > li').on('mouseleave', function(){
         $('header .gnb .depth1 > li').removeClass('over')
     })
+
+
+    $('.news .tab > li').on('click', function(){
+        $('.news .tab > li').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    $('.layer_open').on('click', function(){
+        $('.layer_popup').show()
+    })
+    $('.layer_popup button').on('click', function(){
+        $('.layer_popup').hide()
+    })
 })
