dff906975c8327ef8b295f3f0e119c9acc6de763
SEONHANSOL <goodgirl48@naver.com>
2024-11-08 16:43:19 +0900

    2

---
+++ star/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('header .gnb .depth1 > li').on('mouseenter focusin', function(){
+        $('header .gnb .depth1 > li').removeClass('over')
+        $(this).addClass('over')
+    })
+
+    $('header .gnb .depth1 > li').on('mouseleave', function(){
+        $('header .gnb .depth1 > li').removeClass('over')
+    })
+
+    $('.news .tab > li').on('click', function(){
+        $('.news .tab > li').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    $('.layer_open').on('click', function(){
+        $('.layer_popup').show()
+    })
+    $('.layer_popup button').on('click', function(){
+        $('.layer_popup').hide()
+    })
+
+    let idx = 1
+
+    setInterval(function(){
+        if(idx < 3){
+            idx++
+        }else{
+            idx = 1
+        }
+
+        console.log(idx)
+
+        $('.popup ul li').removeClass('acrive')
+        $('.popup ul li').eq(idx - 1).addClass('active')
+
+    }, 1000)
+    
+})
