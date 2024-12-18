dd43fef9244e8154a863ce7792e809a459a6bd9a
SEONHANSOL <goodgirl48@naver.com>
2024-12-18 16:43:28 +0900

    d
    d

--- space/js/main.js
+++ space/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
 	const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
 		autoplay: {  /* 팝업 자동 실행 */
-			delay: 2500,
+			delay: 5000,
 			disableOnInteraction: true,
 		},
 	
